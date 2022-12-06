74aa9b4a7ef46a32302482a48c4003a9de4a38b3
Wati-Dev <raphaelgrougnetca@gmail.com>
2022-12-06 13:34:27 -0500

    Modif du listbox pour l'affichage des produits

--- TP3-420-14B-FX/FormPointDeVente.xaml.cs
+++ TP3-420-14B-FX/FormPointDeVente.xaml.cs
@@ -1,21 +1,30 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using TP3_420_14B_FX.classes;
 
 namespace TP3_420_14B_FX
 {
+    
+
     /// <summary>
     /// Logique d'interaction pour FormPointDeFente.xaml
     /// </summary>
     public partial class FormPointDeVente : Window
     {
-       
+
+        private List<Produit> _listeProduit;
+
+
         public FormPointDeVente()
         {
             InitializeComponent();
- 
+            _listeProduit = GestionFacture.ObtenirListeProduits();
+            AfficherListeProduits();
         }
         
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -24,6 +33,82 @@ namespace TP3_420_14B_FX
            
         }
 
+        private void AfficherListeProduits()
+        {
+            //_listeProduit = new List<Produit>() { new Produit(0, "123-4567", "Chaussures Cali de UGG", new Categorie(0, "Chaussures"), 150.55M, @"C:\data-420-14B-FX\data-tp3-420-14b\Images\6f6eec5c-3f7b-496d-8ee4-7a3864e4fe2c.jpg") };/*GestionFacture.ObtenirListeProduits();*/
+            //lstProduits.ItemsSource = _listeProduit;
+            foreach (Produit produit in _listeProduit)
+            {
+                Border border = new Border();
+                border.BorderBrush = new SolidColorBrush(Colors.Gray);
+                border.BorderThickness = new Thickness(1);
+                border.CornerRadius = new CornerRadius(5);
+                border.Margin = new Thickness(5);
+                border.Padding = new Thickness(10);
+
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.VerticalAlignment = VerticalAlignment.Center;
+                stackPanel.HorizontalAlignment = HorizontalAlignment.Center;
+                stackPanel.Width = 100;
+                stackPanel.Height = 200;
+
+                BitmapImage bi = new BitmapImage(new Uri(produit.Image));
+                Image imageProd = new Image();
+                imageProd.Source = bi;
+                imageProd.Width = 120;
+                imageProd.Height = 80;
+                imageProd.HorizontalAlignment = HorizontalAlignment.Center;
+
+                TextBlock txtNomProd = new TextBlock();
+                txtNomProd.Text = produit.Nom;
+                txtNomProd.FontSize = 15;
+                txtNomProd.TextWrapping = TextWrapping.Wrap;
+                txtNomProd.TextAlignment = TextAlignment.Left;
+                txtNomProd.Padding = new Thickness(5, 7, 5, 7);
+                txtNomProd.HorizontalAlignment = HorizontalAlignment.Center;
+
+
+                TextBlock txtPrixProd = new TextBlock();
+                txtPrixProd.Text = produit.Prix + " $";
+                txtPrixProd.FontSize = 15;
+                txtPrixProd.FontWeight = FontWeights.Bold;
+                txtPrixProd.Foreground = new SolidColorBrush(Colors.Green);
+                txtPrixProd.TextAlignment = TextAlignment.Right;
+                txtPrixProd.Margin = new Thickness(0, 10, 4, 5);
+                txtPrixProd.Height = 25;
+
+                StackPanel spImgEditSuppr = new StackPanel();
+                spImgEditSuppr.Orientation = Orientation.Horizontal;
+                spImgEditSuppr.HorizontalAlignment= HorizontalAlignment.Center;
+
+                Image imgEdit = new Image();
+                bi = new BitmapImage(new Uri("C:\\Users\\rapha\\Desktop\\TP3 Prog2\\TP3-420-14B-FX\\Resources\\edit.png"));
+                imgEdit.Source = bi;
+                imgEdit.Width = 30;
+
+                Image imgDelete = new Image();
+                bi = new BitmapImage(new Uri("C:\\Users\\rapha\\Desktop\\TP3 Prog2\\TP3-420-14B-FX\\Resources\\delete.png"));
+                imgDelete.Source = bi;
+                imgDelete.Width = 26;
+
+                spImgEditSuppr.Children.Add(imgEdit);
+                spImgEditSuppr.Children.Add(imgDelete);
+
+                stackPanel.Children.Add(imageProd);
+                stackPanel.Children.Add(txtNomProd);
+                stackPanel.Children.Add(txtPrixProd);
+                stackPanel.Children.Add(spImgEditSuppr);
+
+                border.Child= stackPanel;
+
+                wpProduits.Children.Add(border);
+
+
+            }
+            
+            
+        }
+        
         /// <summary>
         /// Permet la création et l'affichage d'un bouton catégorie.
         /// </summary>
@@ -88,9 +173,11 @@ namespace TP3_420_14B_FX
             if (frmProduit.DialogResult == true)
             {
                 ProduitFacture produitFactureNew = new ProduitFacture(frmProduit.ProduitAjoutModif, frmProduit.ProduitAjoutModif.Prix, 1);
+                GestionFacture.AjouterProduit(produitFactureNew.Produit);
+                
             }
-            
-            
+
+
         }
     }
 }
